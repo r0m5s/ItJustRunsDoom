ecb5f79bab0a38279166693dae56feae55030930
romas <romas@pop-os.localdomain>
2023-11-13 22:09:32 +0000

    First Commit

---
+++ config.el
@@ -0,0 +1,92 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+;;(setq user-full-name "John Doe"
+;;      user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+ (setq doom-font (font-spec :family "monospace" :size 17 :weight 'semi-light)
+       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-dracula)
+
+;; ORG CONFIGURATION
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(after! org
+  (setq org-directory "~/.config/doom/org/"
+        org-agenda-files '("~/.config/doom/org/agenda.org")
+        org-hide-emphasis-markers t
+        org-agenda-span 60
+        org-log-done 'time
+        org-ellipsis " ▼ "
+        org-todo-keywords '((sequence
+                             "TODO(t)"
+                             "INPROGRESS(i)"
+                             "WAITING(W)"
+                             "|"
+                             "DONE(d)"
+                             "CANCELLED(c)")))
+)
+
+(custom-set-faces
+ '(org-level-1 ((t (:inherit outline-1 :height 1.3))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.2))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.1))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
+)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type `relative)
+(setq confirm-kill-emacs nil)
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+(setq shell-file-name (executable-find "bash"))
+
+;; Remappings
+
+;; Unset keybinds
+;;(map! :map doom-leader-map "c f" nil)
+
+;; Reassign 'SPC c f' to use LSP format
+(map! :leader
+      (:prefix ("c","+code")
+       :desc "Format code structure" "f" #'lsp-format-buffer))
+
+;; Fold functions instead of using 'z-a'
+(map! "<backtab>" #'evil-toggle-fold)
