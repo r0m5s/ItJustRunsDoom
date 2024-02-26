ee690fb7de10e50e82e343e8aa4275e9364eff1a
romas <romas@pop-os.localdomain>
2024-02-26 21:54:13 +0000

    Added dap config

--- config.el
+++ config.el
@@ -78,6 +78,44 @@
 
 (setq shell-file-name (executable-find "bash"))
 
+;; For packages configs
+
+;; debug for python
+(after! dap-mode
+  (setq dap-python-debugger 'debugpy))
+
+;; keybinds for debugger
+(map! :map dap-mode-map
+      :leader
+      :prefix ("d" . "dap")
+      ;; basics
+      :desc "dap next"          "n" #'dap-next
+      :desc "dap step in"       "i" #'dap-step-in
+      :desc "dap step out"      "o" #'dap-step-out
+      :desc "dap continue"      "c" #'dap-continue
+      :desc "dap hydra"         "h" #'dap-hydra
+      :desc "dap debug restart" "r" #'dap-debug-restart
+      :desc "dap debug"         "s" #'dap-debug
+
+      ;; debug
+      :prefix ("dd" . "Debug")
+      :desc "dap debug recent"  "r" #'dap-debug-recent
+      :desc "dap debug last"    "l" #'dap-debug-last
+
+      ;; eval
+      :prefix ("de" . "Eval")
+      :desc "eval"                "e" #'dap-eval
+      :desc "eval region"         "r" #'dap-eval-region
+      :desc "eval thing at point" "s" #'dap-eval-thing-at-point
+      :desc "add expression"      "a" #'dap-ui-expressions-add
+      :desc "remove expression"   "d" #'dap-ui-expressions-remove
+
+      :prefix ("db" . "Breakpoint")
+      :desc "dap breakpoint toggle"      "b" #'dap-breakpoint-toggle
+      :desc "dap breakpoint condition"   "c" #'dap-breakpoint-condition
+      :desc "dap breakpoint hit count"   "h" #'dap-breakpoint-hit-condition
+      :desc "dap breakpoint log message" "l" #'dap-breakpoint-log-message)
+
 ;; Remappings
 
 ;; Unset keybinds
@@ -91,8 +129,11 @@
 ;; Fold functions instead of using 'z-a'
 (map! "<backtab>" #'evil-toggle-fold)
 
+;; Custom commands
+
 ;; Install dependencies on debian systems
 (defun install-debian-dependencies ()
   "Install debian system dependencies."
   (interactive)
-    (shell-command "sudo apt-get install fd-find ripgrep shfmt shellcheck black"))
+  (let ((default-directory "/sudo::"))
+    (shell-command "apt-get install -y fd-find ripgrep shfmt shellcheck black")))
